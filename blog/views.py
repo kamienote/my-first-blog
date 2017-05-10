9948b61039d334c6413de19c85bdf972f68f0200
kami <bundleformy@gmail.com>
2017-05-10 14:12:56 +0300

    добавил динамики в шаблон post_list.html путем вставки цикла Питона и использования свойств объекта post (title и text в файле models.py)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+# получается здесь использовали словарь для posts, чтобы иметь возможность
+# обращения к нему по ключу 'posts'(а не по индексу, как это было бы для списка)
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
